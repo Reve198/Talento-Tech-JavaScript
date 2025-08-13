cb29b6259ac518cc12fe816624f077a773f7ddf8
Nub3at <miguel097482@gmail.com>
2025-08-12 19:36:00 -0500

    Agregando archivo JS para funcionalidad

---
+++ funcion-flechas.js
@@ -0,0 +1,16 @@
+function nombreDeLaFuncion(n1, n2) {
+    console.log("Los numeros son: " + n1, n2);
+    
+}
+
+const variable = () => {
+
+}  
+
+const ImprimirNombre = (nombre) => {
+
+    return "Hola" + nombre;
+
+}
+resultado = ImprimirNombre("Giovanny")
+console.log(resultado);
